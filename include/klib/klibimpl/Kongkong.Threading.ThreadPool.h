2c298a46c930d4f0e47c1d38520c66e6e783f578
KongkongNT15 <kongkong745837@gmail.com>
2026-03-22 12:32:54 +0900

    ふははは

--- include/klib/klibimpl/Kongkong.Threading.ThreadPool.h
+++ include/klib/klibimpl/Kongkong.Threading.ThreadPool.h
@@ -2,6 +2,7 @@
 #define KLIB_KONGKONG_THRAEDING_THREADPOOL_H
 
 #include "base.h"
+#include "Kongkong.Ranges.IteratorHelper.h"
 #include "Kongkong.Threading.Async.AsyncAction.h"
 
 #if KLIB_ENV_WINDOWS
@@ -33,19 +34,19 @@ namespace klib::Kongkong::Threading
         static void ParallelFor(
             TIterator begin,
             TIterator end,
-            TPredicate pred
+            TPredicate&& pred
         );
 
         template <class TIterator, class TPredicate>
         static Async::AsyncAction ParallelForAsync(
             TIterator begin,
             TIterator end,
-            TPredicate pred
+            TPredicate&& pred
         );
 
         template <class TPredicate>
         static Async::AsyncAction RunAsync(
-            TPredicate pred
+            TPredicate&& pred
         );
     };
 }
@@ -56,35 +57,146 @@ namespace klib::Kongkong::Threading
     void ThreadPool::ParallelFor(
         TIterator begin,
         TIterator end,
-        TPredicate pred
+        TPredicate&& pred
     )
     {
         uint32_t threadCount = ::std::thread::hardware_concurrency();
 
-        
+
     }
 
     template <class TIterator, class TPredicate>
     Async::AsyncAction ThreadPool::ParallelForAsync(
         TIterator begin,
         TIterator end,
-        TPredicate pred
+        TPredicate&& pred
     )
     {
-        return RunAsync(
-            [=]() {
-                ParallelFor(
-                    begin,
-                    end,
-                    pred
-                )
+        struct fArgs {
+            TIterator begin;
+            TIterator end;
+            TPredicate func;
+            ::PTP_WORK work;
+        };
+
+        void (*func)(
+            ::PTP_CALLBACK_INSTANCE,
+            ::PVOID,
+            ::PTP_WORK
+        ) = [](
+            ::PTP_CALLBACK_INSTANCE, 
+            ::PVOID args,
+            ::PTP_WORK
+        )
+        {
+            fArgs fargs = static_cast<fArgs*>(args);
+
+            while (fargs.begin != fargs.end) {
+                fargs.func(
+                    *fargs.begin
+                );
+                ++begin;
             }
+        };
+
+        uint32_t threadCount = ::std::thread::hardware_concurrency();
+
+        ssize_t containerLength =
+            Ranges::IteratorHelper::GetLengthUnsafe(begin, end);
+
+        ssize_t listLength;
+
+        if (threadCount > containerLength) listLength = containerLength;
+        else listLength = threadCount;
+
+        fArgs* stackAllocArray = static_cast<fArgs*>(
+            ::alloca(sizeof(fArgs) * listLength)
         );
+
+        ssize_t c = containerLength % listLength;
+        ssize_t d = containerLength / listLength;
+
+        auto addTask = [](fArgs* target, TIterator b, TIterator e) {
+            new (target) fArgs{
+                b,
+                e,
+                pred,
+                ::CreateThreadpoolWork(func, target, nullptr)
+            };
+        };
+
+        ssize_t stackArrayIndex = 0;
+        TIterator itr = begin;
+
+        // 全スレッドで同じ数だけ実行
+        if (c == 0) {
+            
+            while (itr != end) {
+                TIterator next = Ranges::IteratorHelper::Add(itr, d);
+
+                addTask(
+                    stackAllocArray + stackArrayIndex,
+                    itr,
+                    next
+                );
+
+                ++stackArrayIndex;
+                itr = std::move(next);
+            }
+        }
+        // ふぁ！？っく
+        else {
+            ssize_t n = d * (listLength * (d + 1) - containerLength);
+
+            // 最初のほうのスレッドは少なめに実行
+            for (
+                TIterator e1 = Ranges::IteratorHelper::Add(begin, n);
+                itr != e1;
+                ++stackArrayIndex
+            ) {
+                TIterator next = Ranges::IteratorHelper::Add(itr, d);
+
+                addTask(
+                    stackAllocArray + stackArrayIndex,
+                    itr,
+                    next
+                );
+
+                itr = ::std::move(next);
+            }
+
+            // これ以降は多めに実行
+            d++;
+
+            for (; itr != end; ++stackArrayIndex) {
+                TIterator next = Ranges::IteratorHelper::Add(itr, d);
+
+                addTask(
+                    stackAllocArray + stackArrayIndex,
+                    itr,
+                    next
+                );
+
+                itr = ::std::move(next);
+            }
+        }
+
+        {
+            fArgs* itr1 = stackAllocArray;
+            fArgs* end1 = itr1 + listLength;
+
+            while (itr1 != end1) {
+                ::WaitForThreadpoolWorkCallbacks(itr1->work, FALSE);
+                ::CloseThreadpoolWork(itr1->work);
+
+                itr1->~fArgs();
+            }
+        }
     }
 
     template <class TPredicate>
     Async::AsyncAction ThreadPool::RunAsync(
-        TPredicate pred
+        TPredicate&& pred
     )
     {
 #if KLIB_ENV_WINDOWS
@@ -125,7 +237,7 @@ namespace klib::Kongkong::Threading
             constexpr void await_resume() noexcept {}
         };
 
-        co_await awaiter{ nullptr, ::std::move(pred) };
+        co_await awaiter{ nullptr, ::std::forward(pred) };
 #elif KLIB_COMPILER_APPLE_CLANG
         struct awaiter {
             TPredicate pred;
@@ -146,7 +258,7 @@ namespace klib::Kongkong::Threading
             constexpr void await_resume() noexcept {}
         };
 
-        co_await awaiter{ ::std::move(pred) };
+        co_await awaiter{ ::std::forward(pred) };
 #elif KLIB_ENV_UNIX
 
 #endif
